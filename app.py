49e32338701e996ac36f8c597ecf43fbc315f711
ahmedis123 <68246919+ahmedis123@users.noreply.github.com>
2025-01-16 20:30:27 +0200

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import psycopg2
-from psycopg2 import sql, Error
+from psycopg2 import Error
 import hashlib
 
 app = Flask(__name__)
@@ -9,16 +9,14 @@ app = Flask(__name__)
 def create_connection():
     connection = None
     try:
-       connection = psycopg2.connect("postgresql://admin:0n4a8VaVktozo7N8VKrYvOMAaXaFdOq0@dpg-cu4br6l6l47c73b11lt0-a.oregon-postgres.render.com/election_sys_pnjq")
+        connection = psycopg2.connect("postgresql://admin:0n4a8VaVktozo7N8VKrYvOMAaXaFdOq0@dpg-cu4br6l6l47c73b11lt0-a.oregon-postgres.render.com/election_sys_pnjq")
     except Error as e:
         print(f"Error: {e}")
     return connection
 
 # دالة تشفير كلمة المرور
 def hash_password(password):
-    # دمج كلمة المرور مع Salt
     combined = password + "Elction"
-    # توليد الهاش
     hashed_password = hashlib.sha256(combined.encode()).hexdigest()
     return hashed_password
 
@@ -27,6 +25,13 @@ def create_tables():
     with create_connection() as connection:
         if connection is not None:
             cursor = connection.cursor()
+
+            cursor.execute('''CREATE TABLE IF NOT EXISTS Elections (
+                                ElectionID SERIAL PRIMARY KEY,
+                                ElectionDate TEXT,
+                                ElectionType TEXT,
+                                ElectionStatus TEXT
+                            )''')
 
             cursor.execute('''CREATE TABLE IF NOT EXISTS Voters (
                                 VoterID SERIAL PRIMARY KEY,
@@ -41,7 +46,7 @@ def create_tables():
                                 Phone TEXT UNIQUE
                             )''')
 
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Candidate (
+            cursor.execute('''CREATE TABLE IF NOT EXISTS Candidates (
                                 CandidateID SERIAL PRIMARY KEY,
                                 NationalID TEXT UNIQUE,
                                 CandidateName TEXT,
@@ -52,13 +57,6 @@ def create_tables():
                                 FOREIGN KEY (ElectionID) REFERENCES Elections (ElectionID) ON DELETE CASCADE
                             )''')
 
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Elections (
-                                ElectionID SERIAL PRIMARY KEY,
-                                ElectionDate TEXT,
-                                ElectionType TEXT,
-                                ElectionStatus TEXT
-                            )''')
-
             cursor.execute('''CREATE TABLE IF NOT EXISTS Votes (
                                 VoteID SERIAL PRIMARY KEY,
                                 VoterID INTEGER,
@@ -66,17 +64,17 @@ def create_tables():
                                 CandidateID INTEGER,
                                 ElectionID INTEGER,
                                 FOREIGN KEY (VoterID) REFERENCES Voters (VoterID),
-                                FOREIGN KEY (CandidateID) REFERENCES Candidate (CandidateID),
+                                FOREIGN KEY (CandidateID) REFERENCES Candidates (CandidateID),
                                 FOREIGN KEY (ElectionID) REFERENCES Elections (ElectionID)
                             )''')
 
-            cursor.execute('''CREATE TABLE IF NOT EXISTS Result (
+            cursor.execute('''CREATE TABLE IF NOT EXISTS Results (
                                 ResultID SERIAL PRIMARY KEY,
                                 CountVotes INTEGER,
                                 CandidateID INTEGER,
                                 ResultDate TEXT,
                                 ElectionID INTEGER,
-                                FOREIGN KEY (CandidateID) REFERENCES Candidate (CandidateID),
+                                FOREIGN KEY (CandidateID) REFERENCES Candidates (CandidateID),
                                 FOREIGN KEY (ElectionID) REFERENCES Elections (ElectionID)
                             )''')
 
@@ -87,12 +85,13 @@ def create_tables():
                                 Password TEXT,
                                 Privileges TEXT
                             )''')
+
             connection.commit()
             print("Tables created successfully.")
         else:
             print("Failed to create tables due to database connection error.")
 
-# دالة لإضافة الناخبين مع التحقق من صحة الإدخال
+# دالة لإضافة الناخبين
 @app.route('/voters', methods=['POST'])
 def add_voter():
     data = request.get_json()
@@ -135,7 +134,7 @@ def add_candidate():
         if connection is not None:
             cursor = connection.cursor()
             try:
-                query = '''INSERT INTO Candidate (NationalID, CandidateName, PartyName, Biography, CandidateProgram, ElectionID)
+                query = '''INSERT INTO Candidates (NationalID, CandidateName, PartyName, Biography, CandidateProgram, ElectionID)
                            VALUES (%s, %s, %s, %s, %s, %s)'''
                 cursor.execute(query, (
                     data['NationalID'], data['CandidateName'], data['PartyName'],
@@ -213,7 +212,7 @@ def add_result():
         if connection is not None:
             cursor = connection.cursor()
             try:
-                query = '''INSERT INTO Result (CountVotes, CandidateID, ResultDate, ElectionID)
+                query = '''INSERT INTO Results (CountVotes, CandidateID, ResultDate, ElectionID)
                         VALUES (%s, %s, %s, %s)'''
                 cursor.execute(query, (data['CountVotes'], data['CandidateID'], data['ResultDate'], data['ElectionID']))
                 connection.commit()
@@ -257,7 +256,7 @@ def add_admin():
 def get_voters():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Voters"')
+        cursor.execute('SELECT * FROM Voters')
         rows = cursor.fetchall()
 
         if not rows:
@@ -281,10 +280,10 @@ def get_voters():
 
 # دالة لاسترجاع المرشحين
 @app.route('/candidates', methods=['GET'])
-def get_Candidate():
+def get_candidates():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Candidate"')
+        cursor.execute('SELECT * FROM Candidates')
         candidates = cursor.fetchall()
 
         if not candidates:
@@ -311,7 +310,7 @@ def get_candidates_by_election(election_id):
 
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Candidate" WHERE "ElectionID" = %s', (election_id,))
+        cursor.execute('SELECT * FROM Candidates WHERE ElectionID = %s', (election_id,))
         candidates = cursor.fetchall()
 
         if not candidates:
@@ -335,7 +334,7 @@ def get_candidates_by_election(election_id):
 def get_elections():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Elections"')
+        cursor.execute('SELECT * FROM Elections')
         rows = cursor.fetchall()
 
         if not rows:
@@ -356,7 +355,7 @@ def get_elections():
 def get_votes():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Vote"')
+        cursor.execute('SELECT * FROM Votes')
         rows = cursor.fetchall()
 
         if not rows:
@@ -378,7 +377,7 @@ def get_votes():
 def get_results():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Result"')
+        cursor.execute('SELECT * FROM Results')
         rows = cursor.fetchall()
 
         if not rows:
@@ -400,7 +399,7 @@ def get_results():
 def get_admin():
     with create_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM "Admin"')
+        cursor.execute('SELECT * FROM Admins')
         rows = cursor.fetchall()
 
         if not rows:
@@ -491,7 +490,7 @@ def update_candidate(candidate_id):
         try:
             cursor = conn.cursor()
             query = '''
-                UPDATE Candidate
+                UPDATE Candidates
                 SET NationalID = %s, CandidateName = %s, PartyName = %s, Biography = %s,
                     CandidateProgram = %s, ElectionID = %s
                 WHERE CandidateID = %s
@@ -510,7 +509,7 @@ def update_candidate(candidate_id):
             return jsonify({"error": "Duplicate entry or invalid data"}), 400
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لحذف مرشح
 @app.route('/candidates/<int:candidate_id>', methods=['DELETE'])
 def delete_candidate(candidate_id):
@@ -520,7 +519,7 @@ def delete_candidate(candidate_id):
     with create_connection() as conn:
         try:
             cursor = conn.cursor()
-            query = "DELETE FROM Candidate WHERE CandidateID = %s"
+            query = "DELETE FROM Candidates WHERE CandidateID = %s"
             cursor.execute(query, (candidate_id,))
             conn.commit()
 
@@ -530,7 +529,7 @@ def delete_candidate(candidate_id):
             return jsonify({"message": "Candidate deleted successfully!"}), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لتحديث الانتخابات
 @app.route('/elections/<int:election_id>', methods=['PUT'])
 def update_election(election_id):
@@ -564,7 +563,7 @@ def update_election(election_id):
             return jsonify({"error": "Duplicate entry or invalid data"}), 400
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لحذف الانتخابات
 @app.route('/elections/<int:election_id>', methods=['DELETE'])
 def delete_election(election_id):
@@ -584,7 +583,7 @@ def delete_election(election_id):
             return jsonify({"message": "Election deleted successfully!"}), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لتحديث مدير نظام
 @app.route('/admin/<int:admin_id>', methods=['PUT'])
 def update_admin(admin_id):
@@ -603,7 +602,7 @@ def update_admin(admin_id):
         try:
             cursor = conn.cursor()
             query = '''
-                UPDATE Admin
+                UPDATE Admins
                 SET AdminName = %s, Email = %s, Password = %s, Privileges = %s
                 WHERE AdminID = %s
             '''
@@ -620,7 +619,7 @@ def update_admin(admin_id):
             return jsonify({"error": "Duplicate entry or invalid data"}), 400
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لحذف مدير نظام
 @app.route('/admin/<int:admin_id>', methods=['DELETE'])
 def delete_admin(admin_id):
@@ -630,7 +629,7 @@ def delete_admin(admin_id):
     with create_connection() as conn:
         try:
             cursor = conn.cursor()
-            query = '''DELETE FROM Admin WHERE AdminID = %s'''
+            query = '''DELETE FROM Admins WHERE AdminID = %s'''
             cursor.execute(query, (admin_id,))
             conn.commit()
 
@@ -640,22 +639,21 @@ def delete_admin(admin_id):
             return jsonify({"message": "Admin deleted successfully!"}), 200
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
+
 # دالة لاسترجاع النتائج مرتبة حسب عدد الأصوات
 @app.route('/election_results/<int:election_id>', methods=['GET'])
 def get_election_results(election_id):
     if election_id <= 0:
         return jsonify({"error": "Invalid ElectionID. It must be a positive integer."}), 400
 
-    # الاستعلام عن النتائج
     with create_connection() as conn:
         try:
             cursor = conn.cursor()
 
             query = '''
                 SELECT *
-                FROM Candidate AS c
-                INNER JOIN Result AS r ON c.CandidateID = r.CandidateID
+                FROM Candidates AS c
+                INNER JOIN Results AS r ON c.CandidateID = r.CandidateID
                 WHERE r.ElectionID = %s
             '''
             cursor.execute(query, (election_id,))
@@ -665,7 +663,6 @@ def get_election_results(election_id):
             if not results:
                 return jsonify({"error": "No results found"}), 404
 
-            # تجهيز البيانات للإرجاع
             formatted_results = []
             for row in results:
                 formatted_results.append({
@@ -680,19 +677,15 @@ def get_election_results(election_id):
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
-# دالة لتحديث التصزيت 
-@app.route('/Votes/<int:vote_id>', methods=['PUT'])
+
+# دالة لتحديث التصويت
+@app.route('/votes/<int:vote_id>', methods=['PUT'])
 def update_vote(vote_id):
     if vote_id <= 0:
         return jsonify({"error": "Invalid voteID. It must be a positive integer."}), 400
 
-    # قراءة البيانات المرسلة في الطلب
     data = request.get_json()
-
     updatable_fields = ["ElectionDate", "CandidateID", "ElectionID"]
-
-    # التحقق من وجود الحقول المطلوبة في الطلب
     missing_fields = [field for field in updatable_fields if field not in data or data[field] == ""]
     if missing_fields:
         return jsonify({"error": f"Missing or empty fields: {', '.join(missing_fields)}"}), 400
@@ -700,8 +693,8 @@ def update_vote(vote_id):
     with create_connection() as conn:
         try:
             cursor = conn.cursor()
-            query = '''UPDATE Vote SET ElectionDate = %s, CandidateID = %s, ElectionID = %s
-                    WHERE VoterID = %s'''
+            query = '''UPDATE Votes SET ElectionDate = %s, CandidateID = %s, ElectionID = %s
+                    WHERE VoteID = %s'''
             cursor.execute(query, (
                 data['ElectionDate'], data['CandidateID'], data['ElectionID'], vote_id
             ))
@@ -716,8 +709,8 @@ def update_vote(vote_id):
             return jsonify({"error": "Duplicate entry or invalid data"}), 400
         except Exception as e:
             return jsonify({"error": str(e)}), 500
-            
-#   دالة لتسجيل الدخول
+
+# دالة لتسجيل الدخول
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -737,7 +730,7 @@ def login():
         result = cursor.fetchone()
 
         if result:
-            cursor.execute("SELECT ElectionID FROM Vote WHERE VoterID = %s", (result[0],))
+            cursor.execute("SELECT ElectionID FROM Votes WHERE VoterID = %s", (result[0],))
             vote_result = cursor.fetchall()
             if vote_result:
                 election_id = [row[0] for row in vote_result]        
@@ -755,8 +748,8 @@ def login():
 
     finally:
         connection.close()
-        
-#    دالة لتسجيل الدخول مدير النظام
+
+# دالة لتسجيل الدخول مدير النظام
 @app.route('/login_admin', methods=['POST'])
 def login_admin():
     data = request.get_json()
@@ -770,7 +763,7 @@ def login_admin():
         hashed_password = hash_password(password)
 
         cursor.execute('''
-            SELECT Privileges FROM Admin
+            SELECT Privileges FROM Admins
             WHERE Email = %s AND Password = %s
         ''', (Email, hashed_password))
         result = cursor.fetchone()
@@ -805,25 +798,28 @@ def castVote():
         if not voter:
             return jsonify({"error": "Voter not found"}), 404
 
+        if voter[0]:  # إذا كان الناخب قد صوت بالفعل
+            return jsonify({"error": "Voter has already voted"}), 400
+
         cursor.execute("""
-            INSERT INTO Vote (VoterID, ElectionID, CandidateID, ElectionDate )
+            INSERT INTO Votes (VoterID, ElectionID, CandidateID, ElectionDate )
             VALUES (%s, %s, %s, %s)
         """, (voter_id, election_id, candidate_id, date))
 
         cursor.execute("""
-            SELECT CountVotes FROM Result
+            SELECT CountVotes FROM Results
             WHERE ElectionID = %s AND CandidateID = %s
         """, (election_id, candidate_id))
         result = cursor.fetchone()
         if result:
             cursor.execute("""
-                UPDATE Result
+                UPDATE Results
                 SET CountVotes = CountVotes + 1
                 WHERE ElectionID = %s AND CandidateID = %s
             """, (election_id, candidate_id))
         else:
             cursor.execute("""
-                INSERT INTO Result (ElectionID, CandidateID, CountVotes, ResultDate )
+                INSERT INTO Results (ElectionID, CandidateID, CountVotes, ResultDate )
                 VALUES (%s, %s, 1, %s)
             """, (election_id, candidate_id, date))
 
@@ -842,7 +838,7 @@ def castVote():
 
     finally:
         connection.close()
-        
+
 # تشغيل الخادم
 if __name__ == '__main__':
     create_tables()  # إنشاء الجداول عند بدء التطبيق
